a5f580e048c67e0871d1205ff7af8a19965a522a
Ruslan Naushirbanov <naushirbanov13@gmail.com>
2021-03-18 16:36:08 +0500

    'AddBlogApp'

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+	blog_title = models.CharField(max_length=50)
+	blog_date = models.DateField(auto_now_add=True)
+	blog_text = models.TextField()
+	blog_image = models.ImageField(upload_to='blog_images/')
